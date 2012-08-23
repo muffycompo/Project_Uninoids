5e44b241b7382c4d40498f45dcdb39d342d998e7
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2012-08-23 23:00:38 +0100

    UNINOIDS Beta now available @ http://www.binghamuni.edu.ng/uninoids

--- project_uninoids/views/dashboard_v.php
+++ project_uninoids/views/dashboard_v.php
@@ -11,8 +11,8 @@
                                 <?php if(check_student_regno($this->session->userdata('user_id')) == NULL && $this->session->userdata('role_id') == 1) : ?>
                                     <div class="alert error"><strong>Notice!</strong> Your <strong>Matriculation Number</strong> has not been entered! Update your profile.</div>
                                 <?php endif; ?>
-                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. </p>
-                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. </p>
+                            <p>Welcome to UNINOIDS, an online-based educational facility designed to enable you deliver training on Google products and services.</p>
+                            <p>You can also use UNINOIDS for the teaching and learning of other Products and Services but with emphasis on Institutions running Google Apps for Education.</p>
                             </div>
                         </div>
 				
