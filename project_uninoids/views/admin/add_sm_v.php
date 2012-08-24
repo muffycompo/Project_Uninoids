34225731182d9737b72cce2fd5e9ce2fab76d689
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2012-08-24 09:58:28 +0100

    UNINOIDS Beta now available @ http://www.binghamuni.edu.ng/uninoids

--- project_uninoids/views/admin/add_sm_v.php
+++ project_uninoids/views/admin/add_sm_v.php
@@ -1,29 +1,43 @@
-<div id="container">
-	<h1>New Study Material</h1>
+<!-- main content -->
+<div class="content container">
 
-	<div id="body">
-	
-		<p><?php echo form_open('admin/add_sm'); ?></p>
-		
-		<p>
-			<label>Title <span style="color: red;">*</span></label><br />
-			<?php echo form_input('sm_title'); ?>
-		</p>
-		
-		<p>
-			<label>Website URL <span style="color: red;">*</span></label><br />
-			<?php echo form_input('sm_url'); ?>
-		</p>
-		
-		<p>
-			<label>Curriculum</label><br />
-			<?php echo curriculum_dropdown('curriculum_id'); ?>
-		</p>
-		
-		<p><?php echo form_submit('submit_sm','Add Study Material'); ?></p>
-		
-		<p><?php echo form_close(); ?></p>
-		
-		<p><?php echo anchor('admin/manage_sm','<< Cancel') ?></p>
-	</div>
+    <!-- Add Dashboard Menu -->
+     <?php $this->load->view('partials/sidebar', array('name' => name_from_email($this->session->userdata('email_address')),'nav' => $nav)); ?>
+    		<!-- blog posts -->
+		<div class="twelve columns">
+			
+                    <!--<h1>New Learning Group</h1>-->
+			
+                    <!-- add comment form -->
+                    <div class="cblock empty add_comment_form">
+                        
+                        <?php echo form_open('tutor/add_sm',' class="clearfix ajax_contact_form"'); ?>
+                        	
+                            <div class="twelve columns">
+                                <h1>New Study Material</h1>
+                            </div>
+
+                            <div class="twelve columns">
+                                <label>Learning Group Name: <span class="required_star">*</span></label>
+                                <div><?php echo form_input('sm_title',  set_value('sm_title'),'id="sm_title" class="contact_input"  required'); ?></div>	
+                                
+                                <label>Website URL: <span class="required_star">*</span></label>
+                                <div><?php echo form_input('sm_url',  set_value('sm_url'),'id="sm_url" class="contact_input"  required'); ?></div>	
+                                
+                                <label>Curriculum: <span class="required_star">*</span></label>
+                                <div><?php echo curriculum_dropdown('curriculum_id',  '', NULL,'id="curriculum_id" class="contact_select" required'); ?></div>	
+                                
+                                <input type="submit" name="submit_sm" class="button" value="Add Study Material"/>&nbsp; <?php echo anchor('tutor/manage_sm','Cancel','class="button"'); ?>
+                            </div>
+								
+			<?php echo form_close(); ?>
+                        
+                    <!-- end: add comment form -->
+                    </div>
+                    
+		<!-- end: blog posts -->
+		</div>
+    
+    
+    <!-- end: main content -->
 </div>
